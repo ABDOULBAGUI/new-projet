69b88a3f4d1ce5f50a18cc5f740c0124dbaf33b2
abdoulbagui <abdoulbagui@DESKTOP-P6P21EC>
2017-03-27 22:01:29 +0200

    premier commit du projet

---
+++ src/gestio1/Gestio1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestio1;
+
+/**
+ *
+ * @author abdoulbagui
+ */
+public class Gestio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int x=4,y=8, z=3,  calcul;
+        calcul=x--;
+        System.out.println("x ="+x+"x--"+calcul);
+        calcul= y---1;
+        System.out.println("y= "+y+"y---1 ="+calcul);
+        calcul= z-- -z;
+        System.out.println("z ="+z+"z-- -z="+calcul);
+        calcul=z-z--;
+        System.out.println("z="+z+"z-z--="+calcul);
+      
+    }
+    
+}
