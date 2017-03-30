7a220abaa7f4e1984537201f9eb6b69a02b4b89a
abdoulbagui <abdoulbagui@DESKTOP-P6P21EC>
2017-03-30 17:15:14 +0200

    mes commit

---
+++ abdoul/src/abdoul/point.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package abdoul;
+
+/**
+ *
+ * @author abdoulbagui
+ */
+public class point {
+float x;
+float y;
+public void afficheCoordonner(){
+System.out.println(abscisse);
+System.out.println(ordonné);
+}
+public void main( String arg[]){ 
+point p=new point();
+p.x=2;
+p.y=3;
+p.afficheCoordonner;
+p.calculDistance;
+}
+    
+}
